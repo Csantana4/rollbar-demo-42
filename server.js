d0c5b2bee908d5b4869f54b4b855353f8b41f37b
Christian Santana <christiansantana2110@gmail.com>
2023-08-17 11:11:54 -0600

    rollbar added into demo

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/students', (req, res) => {
+    rollbar.info("Someone accessed the list of students...")
     res.status(200).send(students)
 })
 
@@ -35,20 +36,24 @@ app.post('/api/students', (req, res) => {
    try {
        if (index === -1 && name !== '') {
            students.push(name)
+           rollbar.info("Someone created a new student.")
            res.status(200).send(students)
        } else if (name === ''){
+        rollbar.error("Someone submitted a blank name!")
            res.status(400).send('You must enter a name.')
        } else {
+        rollbar.error("Someone tried to clone student!")
            res.status(400).send('That student already exists.')
        }
    } catch (err) {
+    rollbar.error(err)
        console.log(err)
    }
 })
 
 app.delete('/api/students/:index', (req, res) => {
     const targetIndex = +req.params.index
-    
+    rollbar.warning("Someone deleted a student!") 
     students.splice(targetIndex, 1)
     res.status(200).send(students)
 })
@@ -56,3 +61,6 @@ app.delete('/api/students/:index', (req, res) => {
 const port = process.env.PORT || 5050
 
 app.listen(port, () => console.log(`Server listening on ${port}`))
+
+
+// Rollbar   .info .debug .critical .error .warning
